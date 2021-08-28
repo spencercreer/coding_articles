267a54fedfbf4d262e63aea808438686a08602dc
spencercreer <spencercreer@gmail.com>
2021-08-27 23:43:10 -0700

    updated to MVC structure

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,12 @@
-const mysql = require('mysql')
+const mysql2 = require('mysql2')
+require('dotenv').config()
 
 // Setup database connection
-const connection = mysql.createConnection({
+const connection = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
-    database: 'coding_articles'
+    password: process.env.PASSWORD,
+    database: "tech_blog_db"
 })
 
 // Connect to database
@@ -12,7 +14,7 @@ connection.connect((err) => {
     if(err) {
         throw err
     }
-    console.log('MySql Connected...')
+    console.log('MySQL connected to tech_blog_db')
 })
 
 module.exports = connection
