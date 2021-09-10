87a7e330e82d597e0f603d257a88db8986268f11
spencercreer <spencercreer@gmail.com>
2021-09-09 19:52:47 -0700

    added post route

--- controller/apiRoutes.js
+++ controller/apiRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-const connection = require('../config/connection')
 const Article = require('../models/Article')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
@@ -7,6 +6,20 @@ const Op = Sequelize.Op
 router.get('/articles', (req, res) => {
    Article.findAll()
         .then(articles => res.render('index', {articles}))
+        .catch(err => console.log(err))
+})
+
+router.post('/add', (req, res) =>{
+    let { title, author, body, url, technologies } = req.body
+    Article.create({
+        title,
+        author,
+        body,
+        url,
+        technologies,
+
+    })
+        .then(article => res.redirect('/articles'))
         .catch(err => console.log(err))
 })
 
