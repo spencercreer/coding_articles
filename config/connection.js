707aef51f6cada5770ba35836f4cb0a79b4b7cff
spencercreer <spencercreer@gmail.com>
2021-09-10 05:36:15 -0700

    added jawsDb connection

--- config/connection.js
+++ config/connection.js
@@ -1,33 +1,22 @@
 const Sequelize = require('sequelize')
+let connection
+if (process.env.JAWSDB_URL) {
+    connection = new Sequelize(process.env.JAWSDB_URL)
+} else {
+    connection = new Sequelize(
+        'articles_db',
+        'root',
+        process.env.PASSWORD,
+        {
+        host: 'localhost',
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    })
+}
 
-module.exports = new Sequelize('articles_db', 'root', process.env.PASSWORD, {
-    host: 'localhost',
-    dialect: 'mysql',
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
-})
-
-// const mysql2 = require('mysql2')
-// require('dotenv').config()
-
-// // Setup database connection
-// const connection = mysql2.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: process.env.PASSWORD,
-//     database: "articles_db"
-// })
-
-// // Connect to database
-// connection.connect((err) => {
-//     if(err) {
-//         throw err
-//     }
-//     console.log('MySQL connected to tech_blog_db')
-// })
-
-// module.exports = connection
+module.exports = connection
